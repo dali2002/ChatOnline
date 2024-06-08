fc4db15126e5144c15963f8ecaf55a3a99852b22
dali2002 <80000430+dali2002@users.noreply.github.com>
2024-06-08 15:01:59 +0200

    Add files via upload

---
+++ ChatServer.py
@@ -0,0 +1,56 @@
+import socket
+import threading
+
+# Server setup
+host = '127.0.0.1'
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            if message:
+                print(f"Received message: {message.decode('utf-8')}")
+                broadcast(message)
+            else:
+                raise Exception("Empty message received")
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat!'.encode('utf-8'))
+            nicknames.remove(nickname)
+            print(f'{nickname} has disconnected')
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}')
+
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} joined the chat!'.encode('utf-8'))
+        client.send('Connected to the server!'.encode('utf-8'))
+
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+print('Server is listening...')
+receive()
